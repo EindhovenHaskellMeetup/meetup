445e02c40c57b541532d9d3a69d08d7d8d5ad24e
Damian Nadales <damian.nadales@gmail.com>
2017-04-18 16:22:31 +0200

    Added the second part of the exercises for Chapter 3.

--- courses/programming-in-haskell/pih-exercises/src/Chapter03.hs
+++ courses/programming-in-haskell/pih-exercises/src/Chapter03.hs
@@ -1,6 +1,8 @@
 -- | Exercises for chapter 3.
 
-module Chapter03 (xs0, xs1, xs2, xs3, t0, t1) where
+module Chapter03 (
+  xs0, xs1, xs2, xs3, t0, t1, twice, palindrome, double, pair, swap, second
+  ) where
 
 -- * Exercise 1
 
@@ -23,3 +25,38 @@ t1 = undefined
 
 -- | TODO: define as @[tail,init,reverse]@
 xs3 = undefined
+
+-- * Exercise 2
+
+-- Define the values below and give them types.
+
+-- | Returns the second element of a list.
+--
+-- TODO: give it a type and implement.
+second xs = undefined
+
+-- | Swap the elements of a tuple.
+--
+-- TODO: give it a type and implement.
+swap (x,y) = undefined
+
+-- | Constructs a pair with the given elements, in the order in which the
+-- parameters appear.
+--
+-- TODO: give it a type and implement.
+pair x y = undefined
+
+-- | Multiplies the given argument by 2.
+--
+-- TODO: give it a type and implement.
+double x = undefined
+
+-- | Determine wether the given list is a palindrome.
+--
+-- TODO: give it a type and implement.
+palindrome xs = undefined
+
+-- | Apply the given function twice to the given argument.
+--
+-- TODO: give it a type and implement.
+twice f x = undefined
