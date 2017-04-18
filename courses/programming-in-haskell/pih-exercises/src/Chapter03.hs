f0be8f8809d2e122f41adcf2e1559b9203246f76
Damian Nadales <damian.nadales@gmail.com>
2017-04-18 11:35:50 +0200

    Added Exercise 1 for Chapter 3.

---
+++ courses/programming-in-haskell/pih-exercises/src/Chapter03.hs
@@ -0,0 +1,29 @@
+-- | Exercises for chapter 3.
+
+module Chapter03 where
+
+-- * Exercise 1
+
+-- Define the values below and give them types.
+
+xs0 = [0, 1]
+
+-- | TODO: define as @['a','b','c']@
+xs1 = undefined
+
+-- | TODO: define as @('a','b','c')@
+t0 = undefined
+
+-- | TODO: define as @[(False,'0'),(True,'1')]@
+t1 = undefined
+
+-- | TODO: define as @([False,True],['0','1'])@
+t2 = undefined
+
+-- | TODO: define as @[tail,init,reverse]@
+xs2 = undefined
+
+
+
+
+
