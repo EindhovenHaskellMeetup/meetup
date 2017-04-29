31d30ad3e998ca83d9ab3676be87dfc1a31f4399
Carlos Rodriguez <carlos.rodriguez.vega.c@philips.com>
2017-04-29 19:03:04 +0200

    Fixed some building problems.

--- courses/programming-in-haskell/pih-exercises/test/Chapter04Spec.hs
+++ courses/programming-in-haskell/pih-exercises/test/Chapter04Spec.hs
@@ -4,56 +4,55 @@ module Chapter04Spec where
 
 import           Chapter04       (safetail, safetail', safetail'', or', or'', or''', and', and'')
 import           Test.Hspec      (Spec, describe, it, shouldBe)
-import           Test.QuickCheck (Property, property, (.&.), (===), (==>))
 
-spec :: Specs
+spec :: Spec
 spec = do
   describe "Exercise 1" $ do
 
     it "implements safetail using a conditional expression" $ do
-      safetail [] `shouldBe` []
-      safetail [1, 2] `shouldBe` [2]
+      safetail ([] :: [Int]) `shouldBe` ([] :: [Int])
+      safetail [1, 2 :: Int] `shouldBe` [2 :: Int]
 
     it "implements safetail using guarded equations" $ do
-      safetail' [] `shouldBe` []
-      safetail' [1, 2] `shouldBe` [2]
+      safetail' ([] :: [Int]) `shouldBe` ([] :: [Int])
+      safetail' [1, 2 :: Int] `shouldBe` [2 :: Int]
 
     it "implements safetail using pattern matching" $ do
-      safetail'' [] `shouldBe` []
-      safetail'' [1, 2] `shouldBe` [2]
+      safetail'' ([] :: [Int]) `shouldBe` ([] :: [Int])
+      safetail'' [1, 2 :: Int] `shouldBe` [2 :: Int]
 
   describe "Exercise 2" $ do
 
     it "implements the logical or operator (||) using pattern matching (definition 1)" $ do
-      or' false false `shouldBe` false
-      or' true true `shouldBe` true
-      or' true false `shouldBe` true
-      or' false true `shouldBe` true
+      or' False False `shouldBe` False
+      or' True True `shouldBe` True
+      or' True False `shouldBe` True
+      or' False True `shouldBe` True
 
     it "implements the logical or operator (||) using pattern matching (definition 2)" $ do
-      or'' false false `shouldBe` false
-      or'' true true `shouldBe` true
-      or'' true false `shouldBe` true
-      or'' false true `shouldBe` true
+      or'' False False `shouldBe` False
+      or'' True True `shouldBe` True
+      or'' True False `shouldBe` True
+      or'' False True `shouldBe` True
 
     it "implements the logical or operator (||) using pattern matching (definition 3)" $ do
-      or''' false false `shouldBe` false
-      or''' true true `shouldBe` true
-      or''' true false `shouldBe` true
-      or''' false true `shouldBe` true
+      or''' False False `shouldBe` False
+      or''' True True `shouldBe` True
+      or''' True False `shouldBe` True
+      or''' False True `shouldBe` True
 
   describe "Exercise 3" $ do
 
     it "implements the logical and operator (&&) using a conditional expression)" $ do
-      and' false false `shouldBe` false
-      and' true true `shouldBe` true
-      and' true false `shouldBe` false
-      and' false true `shouldBe` false
+      and' False False `shouldBe` False
+      and' True True `shouldBe` True
+      and' True False `shouldBe` False
+      and' False True `shouldBe` False
 
   describe "Exercise 4" $ do
 
     it "implements the alternative logical and operator (&&) using a conditional expression)" $ do
-      and'' false false `shouldBe` false
-      and'' true true `shouldBe` true
-      and'' true false `shouldBe` false
-      and'' false true `shouldBe` false
+      and'' False False `shouldBe` False
+      and'' True True `shouldBe` True
+      and'' True False `shouldBe` False
+      and'' False True `shouldBe` False
