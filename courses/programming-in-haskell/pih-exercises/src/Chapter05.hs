28c6ecdd9207935d50e8067a8f48bd4ffad7b48d
Damian Nadales <damian.nadales@gmail.com>
2017-05-03 12:56:33 +0200

    Removed the solutions from the master branch.

--- courses/programming-in-haskell/pih-exercises/src/Chapter05.hs
+++ courses/programming-in-haskell/pih-exercises/src/Chapter05.hs
@@ -4,12 +4,12 @@ module Chapter05 (pyths, perfects, scalarproduct) where
 
 -- * Exercise 1
 pyths :: Int -> [(Int,Int,Int)]
-pyths n =  [(x,y,z)| x <- [1..n], y <- [1..n], z <- [x..n], x*x+y*y == z*z]
+pyths = undefined
 
 -- * Exercise 2
 perfects :: Int -> [Int]
-perfects n = [ m | m <- [1..n], sum [f | f <- [1..m-1], m `mod` f == 0] == m]
+perfects = undefined
 
 -- * Exercise 3
 scalarproduct :: [Int] -> [Int] -> [Int]
-scalarproduct xs ys = [ m*n | (m,n) <- zip xs ys]
+scalarproduct = undefined
