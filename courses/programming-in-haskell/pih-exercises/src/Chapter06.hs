969257254ac2b284fa77870826b03ed94fbc66e2
Rian.Wouters <rian.wouters@philips.com>
2017-05-10 14:57:54 +0200

    hide prelude functions

--- courses/programming-in-haskell/pih-exercises/src/Chapter06.hs
+++ courses/programming-in-haskell/pih-exercises/src/Chapter06.hs
@@ -1,29 +1,31 @@
 -- | Exercises for chapter 5.
 
-module Chapter06 (and', concat', replicate', (!!!), elem', merge, msort) where
+module Chapter06 (and, concat, replicate, (!!), elem, merge, msort) where
+
+import Prelude hiding (and, concat, elem, replicate, (!!))
 
 -- * Exercise 1
-and' :: [Bool] -> Bool
-and' [] = True
-and' (False:xs) = False
-and' (True:xs) = and' xs
+and :: [Bool] -> Bool
+and [] = True
+and (False:xs) = False
+and (True:xs) = and xs
 
-concat' :: [[a]] -> [a]
-concat' [] = []
-concat' (xs:xxs) = xs ++ concat' xxs
+concat :: [[a]] -> [a]
+concat [] = []
+concat (xs:xxs) = xs ++ concat xxs
 
-replicate' :: Int -> a -> [a]
-replicate' n x | n == 0 = []
-               | n > 0 = x:(replicate' (n-1) x)
+replicate :: Int -> a -> [a]
+replicate n x | n == 0 = []
+               | n > 0 = x:(replicate (n-1) x)
 
-(!!!) :: [a] -> Int -> a
-(!!!) (x:xs) n
+(!!) :: [a] -> Int -> a
+(!!) (x:xs) n
     | n == 0 = x
-    | n > 0 = (!!!) xs (n-1)
+    | n > 0 = (!!) xs (n-1)
 
-elem' :: Eq a => a -> [a] -> Bool
-elem' x [] = False
-elem' x (y:xs) = (x == y) || (elem x xs)
+elem :: Eq a => a -> [a] -> Bool
+elem x [] = False
+elem x (y:xs) = (x == y) || (elem x xs)
 
 -- * Exercise 2
 merge :: Ord a => [a] -> [a] -> [a]
--- courses/programming-in-haskell/pih-exercises/test/Chapter06Spec.hs
+++ courses/programming-in-haskell/pih-exercises/test/Chapter06Spec.hs
@@ -2,7 +2,7 @@
 
 module Chapter06Spec where
 
-import Chapter06 (and', concat', replicate', (!!!), elem', merge, msort)
+import Chapter06 as C6
 import Test.Hspec (Spec, describe, it, shouldBe)
 
 spec :: Spec
@@ -12,84 +12,84 @@ spec = do
     describe "returns true iff all logical values in a list are true" $ do
 
         it "returns True for the empty list" $ do
-          and' ([]:: [Bool]) `shouldBe` True
+          C6.and ([]:: [Bool]) `shouldBe` True
 
         it "returns True if only True is in the list" $ do
-          and' [True] `shouldBe` True
+          C6.and [True] `shouldBe` True
 
         it "returns False if only False is in the list" $ do
-          and' [False] `shouldBe` False
+          C6.and [False] `shouldBe` False
 
         it "returns False if any value is False" $ do
-          and' [True, False, False] `shouldBe` False
-          and' [False, True, True] `shouldBe` False
+          C6.and [True, False, False] `shouldBe` False
+          C6.and [False, True, True] `shouldBe` False
 
         it "returns True if all values are True" $ do
-          and' [True, True, True ] `shouldBe` True
+          C6.and [True, True, True ] `shouldBe` True
 
     describe "concatenates a list of lists" $ do
 
         it "returns empty list for the empty list" $ do
-          concat' ([] :: [[Int]]) `shouldBe` []
+          C6.concat ([] :: [[Int]]) `shouldBe` []
 
         it "returns the list for a list of only one list" $ do
-          concat' ([[1,2,3]] :: [[Int]]) `shouldBe` [1,2,3]
+          C6.concat ([[1,2,3]] :: [[Int]]) `shouldBe` [1,2,3]
 
         it "returns the concatenation of the list in the list" $ do
-          concat' ([[1,2,3], [4,5,6], [7,8,9]] :: [[Int]]) `shouldBe` [1,2,3,4,5,6,7,8,9]
+          C6.concat ([[1,2,3], [4,5,6], [7,8,9]] :: [[Int]]) `shouldBe` [1,2,3,4,5,6,7,8,9]
 
     describe "produces a list with n identical elements" $ do
 
         it "returns empty list for n = 0" $ do
-          replicate' 0 True `shouldBe` []
-          replicate' 0 'a' `shouldBe` []
+          C6.replicate 0 True `shouldBe` []
+          C6.replicate 0 'a' `shouldBe` []
 
         it "returns a list with 1 element for n = 1" $ do
-          replicate' 1 13 `shouldBe` [13]
+          C6.replicate 1 13 `shouldBe` [13]
 
         it "returns a list with n times the same element" $ do
-          replicate' 3 'a' `shouldBe` "aaa"
-          replicate' 2 (13::Int) `shouldBe` [13,13]
+          C6.replicate 3 'a' `shouldBe` "aaa"
+          C6.replicate 2 (13::Int) `shouldBe` [13,13]
 
     describe "selects the nth element of a list" $ do
 
         it "returns the element if n=0 and there is only one element" $ do
-          (!!!) ['a'] 0 `shouldBe` 'a'
+          (C6.!!) ['a'] 0 `shouldBe` 'a'
 
         it "returns the element if nth element" $ do
-          (!!!) "abcd" 2 `shouldBe` 'c'
+          (C6.!!) "abcd" 2 `shouldBe` 'c'
 
     describe "decides if an element is in a list" $ do
 
         it "returns False if the list is empty" $ do
-          (elem') 'a' [] `shouldBe` False
+          C6.elem 'a' [] `shouldBe` False
 
         it "returns True if the element is in the list" $ do
-          (elem') 'a' "dcba" `shouldBe` True
+          C6.elem 'a' "dcba" `shouldBe` True
 
         it "returns False if the element is not in the list" $ do
-          (elem') 'e' "dcba" `shouldBe` False
+          C6.elem 'e' "dcba" `shouldBe` False
 
   describe "Exercise 2: define a recursive function that" $ do
 
     describe "merges two sorted lists of values to give a single sorted list" $ do
 
         it "returns the empty list if both lists are empty" $ do
-          merge ([]::[Int]) ([]::[Int]) `shouldBe` []
+          C6.merge ([]::[Int]) ([]::[Int]) `shouldBe` []
 
         it "returns the other list if one of both lists is empty" $ do
-          merge "foo" "" `shouldBe` "foo"
-          merge "" "foo" `shouldBe` "foo"
+          C6.merge "foo" "" `shouldBe` "foo"
+          C6.merge "" "foo" `shouldBe` "foo"
 
         it "merges the two lists" $ do
-          merge "foo" "abr" `shouldBe` "abfoor"
+          C6.merge "foo" "abr" `shouldBe` "abfoor"
 
   describe "Exercise 3: define a recursive function that implements merge sort" $ do
         it "returns the empty list if the list is empty" $ do
-          msort ([]::[Int]) `shouldBe` []
+          C6.msort ([]::[Int]) `shouldBe` []
 
         it "returns the list if the has length 1" $ do
-          msort "a" `shouldBe` "a"
+          C6.msort "a" `shouldBe` "a"
 
         it "returns the sorted list" $ do
-          msort "the quick brown fox jumps over the lazy dog" `shouldBe` "        abcdeeefghhijklmnoooopqrrsttuuvwxyz"
+          C6.msort "the quick brown fox jumps over the lazy dog" `shouldBe` "        abcdeeefghhijklmnoooopqrrsttuuvwxyz"
