4a6f00480c1850b3ec3a169cb6eed0b0e15260fc
Rian.Wouters <rian.wouters@philips.com>
2017-05-10 17:49:56 +0200

    add map f

--- courses/programming-in-haskell/pih-exercises/src/Chapter07.hs
+++ courses/programming-in-haskell/pih-exercises/src/Chapter07.hs
@@ -1,12 +1,17 @@
 -- | Exercises for chapter 7.
 
-module Chapter07 (comprehension) where
+module Chapter07 (comprehension, map') where
 
 -- * Exercise 1
 
--- ?
+answer::[Char]
+answer = "curried functioins"
 
 -- * Exercise 2
 
 comprehension :: (a -> b) -> (a -> Bool) -> [a] -> [b]
 comprehension f p = (map f) . (filter p)
+
+-- * Exercise 3
+map' :: ((a -> b) -> [a] -> [b])
+map' f = foldr ((:) . f) []
--- courses/programming-in-haskell/pih-exercises/test/Chapter07Spec.hs
+++ courses/programming-in-haskell/pih-exercises/test/Chapter07Spec.hs
@@ -1,16 +1,26 @@
 module Chapter07Spec where
 
-import Chapter07 (comprehension)
+import Chapter07 (comprehension, map')
 import Test.Hspec (Spec, describe, it, shouldBe)
 
 spec :: Spec
 spec = do
   describe "Exercise 1: What are higher-order functions that return functions as results better known as?" $ do
 
-    it "has no tests" $ do
-      True `shouldBe` True
+    it "has the correct answer" $ do
+      answer `shouldBe` ""
 
   describe "Exercise 2: Express the comprehension [f x | x <- xs, p x] using map and filter" $ do
 
       it "returns the empty list if both lists are empty" $ do
           comprehension (*2) (>4) [1,2,3,4,5,6,7,8] `shouldBe` [10,12,14,16]
+
+  describe "Exercise 3: Redefine map f and filter p using foldr" $ do
+
+      describe "map f" $ do
+
+        it "returns empty list for empty list" $ do
+            map' (*2) [] `shouldBe` []
+
+        it "applies f to each element in the list " $ do
+            map' (*2) [1,2,3,4] `shouldBe` [2,4,6,8]
