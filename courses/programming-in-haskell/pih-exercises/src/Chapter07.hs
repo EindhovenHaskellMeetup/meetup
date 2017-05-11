88143086248b248c43e0358d881040e16cde32e7
Damian Nadales <damian.nadales@gmail.com>
2017-05-11 16:28:13 +0200

    - Added the files submitted to Rian to the master branch. - Removed the solutions.

---
+++ courses/programming-in-haskell/pih-exercises/src/Chapter07.hs
@@ -0,0 +1,23 @@
+-- | Exercises for chapter 7.
+
+module Chapter07 (answer, comprehension, map', filter', filter'') where
+
+-- * Exercise 1
+
+answer::[Char]
+answer = undefined
+
+-- * Exercise 2
+
+comprehension :: (a -> b) -> (a -> Bool) -> [a] -> [b]
+comprehension = undefined
+
+-- * Exercise 3
+map' :: ((a -> b) -> [a] -> [b])
+map' = undefined
+
+filter' :: ((a -> Bool) -> [a] -> [a])
+filter' = undefined
+
+filter'' :: ((a -> Bool) -> [a] -> [a])
+filter'' = undefined
---
+++ courses/programming-in-haskell/pih-exercises/test/Chapter07Spec.hs
@@ -0,0 +1,40 @@
+module Chapter07Spec where
+
+import Chapter07 (answer, comprehension, map', filter', filter'')
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+spec :: Spec
+spec = do
+  describe "Exercise 1: What are higher-order functions that return functions as results better known as?" $ do
+
+    it "has the correct answer" $ do
+      answer `shouldBe` "curried functions"
+
+  describe "Exercise 2: Express the comprehension [f x | x <- xs, p x] using map and filter" $ do
+
+      it "returns the empty list if the lists is empty" $ do
+          comprehension (*2) (>4) [] `shouldBe` []
+
+      it "returns the comprehension results" $ do
+          comprehension (*2) (>4) [1,2,3,4,5,6,7,8] `shouldBe` [10,12,14,16]
+
+  describe "Exercise 3: Redefine map f and filter p using foldr" $ do
+
+      describe "map f" $ do
+
+        it "returns empty list for empty list" $ do
+            map' (*2) [] `shouldBe` []
+
+        it "applies f to each element in the list " $ do
+            map' (*2) [1,2,3,4] `shouldBe` [2,4,6,8]
+
+      describe "filter p" $ do
+
+        it "returns empty list for empty list" $ do
+            filter' (>4) [] `shouldBe` []
+            filter'' (>4) [] `shouldBe` []
+
+        it "returns only elements matching the predicate in the same order" $ do
+            filter' (>4) [1,5,2,6,3,7,8] `shouldBe` [5,6,7,8]
+            filter'' (>4) [1,5,2,6,3,7,8] `shouldBe` [5,6,7,8]
+
